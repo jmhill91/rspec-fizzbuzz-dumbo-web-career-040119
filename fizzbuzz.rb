ffb086c3a0a5b6388aa624de5a7cb03f9d6b106e
John Hill <john.m.hill91@gmail.com>
2019-03-12 03:34:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(num)
   # if num % 3 ==0 || num % 5==0
   if num % 3==0 
   "Fizz"
-#   elsif num % 5==0
-#   "Buzz"
+  elsif num % 5==0
+  "Buzz"
 # end
 #   if num% 3==0 && num % 5==0
 #   "FizzBuzz"
